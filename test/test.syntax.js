7f3e383ec820cdd03cb78985ad69e877a0c14050
nemo <nemo@oncoding.me>
2017-08-06 22:43:10 +0800

    test:Add the exception test branch

--- test/test.syntax.js
+++ test/test.syntax.js
@@ -8,6 +8,17 @@ var sumShouldArry = [152, 175];
 
 describe('Syntax to call the apis', function(){
   describe('CheckSum', function(){
+    it('Should throw an exception when input stream is null', function(){
+      (function(){
+        crc16.checkSum(null)
+      }).should.throw();
+    })
+    it('Should throw an exception when input stream is invalid', function(){
+      (function(){
+        crc16.checkSum('qweewq', 'hex')
+      }).should.throw();
+    })
+
     it('Stream input should be a hex string and return a string sum by default', function(){
       var sum = crc16.checkSum(stream);
       sum.should.equal(sumShouldStr);
@@ -27,6 +38,17 @@ describe('Syntax to call the apis', function(){
   })
 
   describe('verifySum', function(){
+    it('Should throw an exception when input stream is null', function(){
+      (function(){
+        crc16.verifySum(null)
+      }).should.throw();
+    })
+    it('Should throw an exception when input stream is invalid', function(){
+      (function(){
+        crc16.verifySum('qweewq', 'hex')
+      }).should.throw();
+    })
+
     it('Stream input should be a hex string by default', function(){
       var isValid = crc16.verifySum(stream + sumShouldStr);
       isValid.should.equal(true);
@@ -38,6 +60,11 @@ describe('Syntax to call the apis', function(){
     it('Stream input can be a buffer', function(){
       var isValid = crc16.verifySum(util.bufferFactory(stream + sumShouldStr, 'hex'));
       isValid.should.equal(true);
+    })
+
+    it('Should return false when the sum is not matched the stream input', function(){
+      var isValid = crc16.verifySum(stream + '7878');
+      isValid.should.equal(false);
     })
   })
 })
