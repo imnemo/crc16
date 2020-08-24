8ca274476450d4dbb1167cb9662c8c5998865447
imnemo <291400733@qq.com>
2020-08-24 19:25:21 +0800

    doc: update .d.ts
    Signed-off-by: imnemo <291400733@qq.com>

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,7 @@
+type RetType = 'hex' | 'array' | 'int' | 'buffer';
+
 type CheckSumOption = {
-  retType?: string = 'hex' | 'array' | 'int' | 'buffer'
+  retType?: RetType
 } & {
   (key?: string): any
 };
@@ -11,16 +13,22 @@ export function checkSum(input: string, option: {retType: 'hex'}): string;
 export function checkSum(input: string, option: {retType: 'array'}): [];
 export function checkSum(input: string, option: {retType: 'int'}): number;
 export function checkSum(input: string, option: {retType: 'buffer'}): Buffer;
+export function checkSum(input: string, option: {retType: RetType}): string;
 
 export function checkSum(input: string, encoding: BufferEncoding = 'hex'): string;
 export function checkSum(input: string, encoding: BufferEncoding = 'hex', option?: {retType: 'hex'}): string;
 export function checkSum(input: string, encoding: BufferEncoding = 'hex', option?: {retType: 'array'}): [];
 export function checkSum(input: string, encoding: BufferEncoding = 'hex', option?: {retType: 'int'}): number;
 export function checkSum(input: string, encoding: BufferEncoding = 'hex', option?: {retType: 'buffer'}): Buffer;
+export function checkSum(input: string, encoding: BufferEncoding = 'hex', option?: {retType: RetType}): Buffer;
 
 export function checkSum(input: Buffer): string;
 export function checkSum(input: Buffer, option?: {retType: 'hex'}): string;
 export function checkSum(input: Buffer, option?: {retType: 'array'}): string;
 export function checkSum(input: Buffer, option?: {retType: 'int'}): string;
 export function checkSum(input: Buffer, option?: {retType: 'buffer'}): string;
+export function checkSum(input: Buffer, option?: {retType: RetType}): string;
+
+export function verifySum(input: string, encoding?: string): boolean;
+export function verifySum(input: Buffer): boolean;
 
