cd6aea782c33c3911a628ba11d9e7305a6ce7470
imnemo <291400733@qq.com>
2020-08-26 16:43:14 +0800

    doc: update .d.ts
    Signed-off-by: imnemo <291400733@qq.com>

--- index.d.ts
+++ index.d.ts
@@ -8,26 +8,23 @@ type CheckSumOption = {
 
 // export function checkSum(input: string, encoding?: string | CheckSumOption, option?: CheckSumOption): string | number | [] | Buffer;
 
-export function checkSum(input: string): string;
 export function checkSum(input: string, option: {retType: 'hex'}): string;
 export function checkSum(input: string, option: {retType: 'array'}): [];
 export function checkSum(input: string, option: {retType: 'int'}): number;
 export function checkSum(input: string, option: {retType: 'buffer'}): Buffer;
-export function checkSum(input: string, option: {retType: RetType}): string;
+export function checkSum(input: string, option?: {retType: any}): string;
 
-export function checkSum(input: string, encoding: BufferEncoding = 'hex'): string;
-export function checkSum(input: string, encoding: BufferEncoding = 'hex', option?: {retType: 'hex'}): string;
-export function checkSum(input: string, encoding: BufferEncoding = 'hex', option?: {retType: 'array'}): [];
-export function checkSum(input: string, encoding: BufferEncoding = 'hex', option?: {retType: 'int'}): number;
-export function checkSum(input: string, encoding: BufferEncoding = 'hex', option?: {retType: 'buffer'}): Buffer;
-export function checkSum(input: string, encoding: BufferEncoding = 'hex', option?: {retType: RetType}): Buffer;
+export function checkSum(input: string, encoding: BufferEncoding, option: {retType: 'hex'}): string;
+export function checkSum(input: string, encoding: BufferEncoding, option: {retType: 'array'}): [];
+export function checkSum(input: string, encoding: BufferEncoding, option: {retType: 'int'}): number;
+export function checkSum(input: string, encoding: BufferEncoding, option: {retType: 'buffer'}): Buffer;
+export function checkSum(input: string, encoding: BufferEncoding, option?: {retType: any}): string;
 
-export function checkSum(input: Buffer): string;
-export function checkSum(input: Buffer, option?: {retType: 'hex'}): string;
-export function checkSum(input: Buffer, option?: {retType: 'array'}): string;
-export function checkSum(input: Buffer, option?: {retType: 'int'}): string;
-export function checkSum(input: Buffer, option?: {retType: 'buffer'}): string;
-export function checkSum(input: Buffer, option?: {retType: RetType}): string;
+export function checkSum(input: Buffer, option: {retType: 'hex'}): string;
+export function checkSum(input: Buffer, option: {retType: 'array'}): [];
+export function checkSum(input: Buffer, option: {retType: 'int'}): number;
+export function checkSum(input: Buffer, option: {retType: 'buffer'}): Buffer;
+export function checkSum(input: Buffer, option?: {retType: any}): string;
 
 export function verifySum(input: string, encoding?: string): boolean;
 export function verifySum(input: Buffer): boolean;
